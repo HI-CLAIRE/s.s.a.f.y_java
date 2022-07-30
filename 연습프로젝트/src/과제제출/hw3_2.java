1feb9552ab7ad65e00e2edf00d7afa692099b05b
gimgyuyeon <8080claire@naver.com>
2022-07-30 23:17:34 +0900

    220730

--- 연습프로젝트/src/과제제출/hw3_2.java
+++ 연습프로젝트/src/과제제출/hw3_2.java
@@ -3,11 +3,34 @@ package 과제제출;
 import java.util.Scanner;
 
 public class hw3_2 {
+	public static int findMax(int[][] map, int i, int j, int N) {
+		int max = 0;
+		
+		
+		return max;
+	}
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int N = sc.nextInt();
-		for(int i=0)
+		int[][] map = new int[N][N];
+		for(int i=0;i<N;i++) {
+			for(int j=0; j<N; j++) {
+				map[i][j] = sc.nextInt();
+			}
+		}
+		int len = 0;
+		int max = 0;
+		for(int i=0; i<N; i++) {
+			for(int j=0;j<N;j++) {
+				if(map[i][j] == 1) {
+					//오른쪽 탐색시작
+					
+					if(map[i][j+1] == 1)
+						max = len;
+				}
+			}
+		}
 
 	}
 
