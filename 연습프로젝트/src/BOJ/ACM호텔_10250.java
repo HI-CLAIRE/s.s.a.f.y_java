32d6232f515c5a97dfd2a3b4ce2e64826cdc9fca
gimgyuyeon <8080claire@naver.com>
2022-08-09 10:53:08 +0900

    220809

---
+++ 연습프로젝트/src/BOJ/ACM호텔_10250.java
@@ -0,0 +1,10 @@
+package BOJ;
+
+public class ACM호텔_10250 {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
---
+++ 연습프로젝트/src/BOJ/자리배정_10157.java
@@ -0,0 +1,20 @@
+package BOJ;
+
+import java.util.Scanner;
+
+public class 자리배정_10157 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int X = sc.nextInt();
+		int Y = sc.nextInt();
+		int K = sc.nextInt();
+		int[][] map = new int[X][Y];
+		
+		//좌
+		
+		sc.close();
+	}
+
+}
