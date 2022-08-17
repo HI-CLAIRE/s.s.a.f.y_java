6224fdb30b46aca4d36128f50d57157f7277dff6
gimgyuyeon <8080claire@naver.com>
2022-08-17 19:06:49 +0900

    220817

--- 연습프로젝트/src/BOJ/오목2.java
+++ 연습프로젝트/src/BOJ/오목2.java
@@ -25,7 +25,7 @@ public class 오목2 {
 			for(int j=0; j<19; j++) {
 				if(map[i][j] != 0) {
 					for(int d=0; d<4; d++) {
-						if(isomoq(i, j, d, map[i][j] , 0) == 1) {
+						if(isomoq(i, j, d, map[i][j] , -1)) {
 							System.out.println(map[i][j]);
 							System.out.printf("%d %d\n", i + 1, j + 1);
 							return;
@@ -38,20 +38,32 @@ public class 오목2 {
 		
 	}
 
-	static int isomoq(int x, int y, int d, int color, int cnt) {
-		int nx = x + dx[d];
-		int ny = y + dy[d];
 
-		if(cnt == 5)
-			return 1;
-
-		if(map[nx][ny] == color) {
-			if(isomoq(nx, ny, d, color, cnt + 1) == 1);
-				return 1;  
-		}
+	//재귀함수
+	//cnt -1, 5 일 때는 바둑알 다섯개의 앞 뒤 하나씩을 확인해줌
+	static boolean isomoq(int x, int y, int d, int color, int cnt) {
 		
-		return -1;
-	}
-	
+		int nx = x + dx[d] * cnt;
+		int ny = y + dy[d] * cnt;
 
+		//탈출조건, 바둑알 다섯개의 색깔이 같고, 앞 뒤로 하나씩은 다른 색깔이거나 바둑알이 없을 때.
+		if(cnt == 6)
+			return true;
+
+		//바둑판의 범위안에 있는지
+		if(nx < 0 || ny < 0 || nx > 18 || ny > 18) {
+			if (cnt == -1 || cnt == 5)
+				return isomoq(x, y, d, color, cnt + 1);			//앞 뒤 바둑알의 경우 바둑판을 초과하는 범위라도 괜찮으니 재귀 진행
+			return false;
+		}
+
+		//색깔판단
+		if(map[nx][ny] == color && (cnt == -1 || cnt == 5))
+			return false;
+		else if(map[nx][ny] != color && (0 <= cnt && cnt <= 4))
+			return false;  
+		
+		//모두 만족할 시 재귀 진행
+		return isomoq(x, y, d, color, cnt + 1);
+	}
 }
--- 연습프로젝트/src/BOJ/오목_2615.java
+++ 연습프로젝트/src/BOJ/오목_2615.java
@@ -6,12 +6,10 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class 오목_2615 {
-
 	static int[][] map = new int[19][19];
 	static int[] dx = {1, 1, 0, -1}; // 시계반대방향 
 	static int[] dy = {0, 1, 1, 1};
 	public static void main(String[] args) throws IOException {
-		
 		// 입력 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		for(int i=0; i<19; i++) {
@@ -19,13 +17,11 @@ public class 오목_2615 {
 			for(int j=0; j<19; j++)
 				map[i][j] = Integer.parseInt(st.nextToken());
 		}
-		
-		
 		for(int i=0; i<19; i++) {
 			for(int j=0; j<19; j++) {
 				if(map[i][j] != 0) {
 					for(int d=0; d<4; d++) {
-						if(isomoq(i, j, d, map[i][j], 0)) {
+						if(isomoq(i, j, d, map[i][j], -1)) {
 							System.out.println(map[i][j]);
 							System.out.print((i+1) + " " + (j+1));
 							return;
@@ -36,21 +32,22 @@ public class 오목_2615 {
 		}
 		System.out.println(0);
 	}
-	
 	//오목인지 확인 
-	static boolean isomoq(int x, int y, int d, int color, int cnt) {
-		int nx = x + dx[d];
-		int ny = y + dy[d];
-		if(cnt == 4)
+	static boolean isomoq(int x, int y, int d, int color, int pos) {
+		int nx = x + dx[d] * pos;
+		int ny = y + dy[d] * pos;
+		if(pos == 6)
 			return true;
-		
-		if(map[nx][ny] == color && isomoq(nx, ny, d, color, cnt + 1)) {
-			return true;
+		if(nx>=19 || ny>=19 || nx<0 || ny<0) {
+			if(pos == -1 || pos == 5)
+				return isomoq(x, y, d, color, pos + 1);
+			return false;
 		}
-		
-		if(nx>=19 || ny>=19 || nx<0 || ny<0)
+		if(map[nx][ny] == color && (pos == -1 || pos == 5))
+			return false;
+		else if(map[nx][ny] != color && (0 <= pos && pos <= 4))
 			return false;
 		
-		return false;
+		return isomoq(x, y, d, color, pos + 1);
 	}
 }
