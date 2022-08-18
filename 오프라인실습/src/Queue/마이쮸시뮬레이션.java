f97a7bd461e7fb1fdb8b93668bc315ba42fb16d2
김규연 <8080claire@naver.com>
2022-08-18 13:27:40 +0900

    220818

---
+++ 오프라인실습/src/Queue/마이쮸시뮬레이션.java
@@ -0,0 +1,28 @@
+package Queue;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class 마이쮸시뮬레이션 {
+	static class Person {
+		int num;
+		int cnt;
+		
+		public Person(int num, int cnt) {
+			this.num = num;
+			this.cnt = cnt;
+		}
+	}
+	
+	public static void main(String[] args) {
+		Queue<Person> queue = new LinkedList<>();
+		int N = 20;
+		int pNum = 1;
+		
+		while(N > 0) {
+			
+		}
+
+	}
+
+}
