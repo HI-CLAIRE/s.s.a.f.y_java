07304f8b5d0b4a012ad736b89efd5443f0abc20d
김규연 <8080claire@naver.com>
2022-08-25 17:52:57 +0900

    220825

--- ProblemSolving/src/BOJ/배열돌리기1_16926.java
+++ ProblemSolving/src/BOJ/배열돌리기1_16926.java
@@ -1,57 +1,53 @@
 package BOJ;
 
+import java.io.IOException;
 import java.util.Scanner;
 
 public class 배열돌리기1_16926 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		Scanner sc = new Scanner(System.in);
-
 		int M = sc.nextInt();
 		int N = sc.nextInt();
 		int R = sc.nextInt();
-		
 		int[][] map = new int[M][N];
-		
 		for(int i=0; i<M; i++)
 			for(int j=0; j<N; j++)
 				map[i][j] = sc.nextInt();
+		int[] dx = {-1, 0, 1, 0};
+		int[] dy = {0, -1, 0, 1};
 		
-		// 땡겨줄거니까 방향이 반대로 되야함.
-		int[] dx = {0, 1, 0, -1};
-		int[] dy = {1, 0, -1, 0};
-		// 위에거 수정 필요.
-		
-		int layer = 0;
 		int x=0, y=0, d=0;
-		
-		for(int m=M, n=N; m/2>0 || n/2>0; m--, n--, layer++) {
+		int min = Math.min(M, N);
+		for(int layer = 0; layer<min/2;layer++) {
 			x = layer; y = layer;
-			while(R-- > 0) {
-				int tmp = map[layer][layer]; // 처음거 저장하고 마지막원소 땡겨서 덮어쓰기 
+			for(int r=0; r<R; r++) {
+				int tmp = map[layer][layer];
 				while(true) {
 					int nx = x + dx[d];
 					int ny = y + dy[d];
 					
-					if(ny>=m || nx>=n || ny<layer || nx<layer) {
+					if(ny>=M-layer || nx>=N-layer || ny<layer || nx<layer) {
 						d = (d+1) % 4;
 						continue;
 					}
-					// 땡겨주기.
-					
+					map[y][x] = map[ny][nx];
 					x = nx; y = ny;
-					
-					
-					// 탈출조건.
-					if(nx == layer && ny == layer) break;
+					if(nx == layer && ny == layer) {
+						map[layer+1][layer] = tmp;
+						break;
+					}
 				}
-				
 			}
 		}
-		
-		// 출력.
-		
-		
+		StringBuilder sb = new StringBuilder();
+		for(int i=0; i<M; i++) {
+			for(int j=0; j<N; j++) {
+				sb.append(map[i][j]).append(" ");
+			}
+			sb.append("\n");
+		}
+		System.out.println(sb.toString());
+		sc.close();
 	}
-
 }
