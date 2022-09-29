2ab5b5fa88b66e5d3db63a0446b17332ff79cdef
gyuyekim <8080claire@naver.com>
2022-09-29 17:56:36 +0900

    2200929

--- java/ProblemSolving/src/S2/아기상어2_17086.java
+++ java/ProblemSolving/src/S2/아기상어2_17086.java
@@ -6,12 +6,11 @@ import java.util.Scanner;
 
 public class 아기상어2_17086 {
 
-	public static class Water {
+	public static class Node {
 		int r;
 		int c;
 		int cnt;
-		
-		public Water(int r, int c, int cnt) {
+		public Node(int r, int c, int cnt) {
 			super();
 			this.r = r;
 			this.c = c;
@@ -20,11 +19,11 @@ public class 아기상어2_17086 {
 		
 	}
 	
+	static int[][] map;
+	static int N, M, max;
+	static boolean[][] visited;
 	static int[] dr = {-1, -1, 0, 1, 1, 1, 0, -1};
 	static int[] dc = {0, 1, 1, 1, 0, -1, -1, -1};
-	static int[][] map;
-	static boolean[][] visited;
-	static int N, M, max;
 	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
@@ -34,54 +33,57 @@ public class 아기상어2_17086 {
 		map = new int[N][M];
 		visited = new boolean[N][M];
 		
-		for(int i=0; i<N; i++) {
-			for(int j=0; j<M; j++) {
-				map[i][j] = sc.nextInt();
+		for(int r=0; r<N; r++) {
+			for(int c=0; c<M; c++) {
+				map[r][c] = sc.nextInt();
 			}
 		}
-		
 		max = 0;
-		
-		for(int i=0; i<N; i++) {
-			for(int j=0; j<M; j++) {
-				if(!visited[i][j] && map[i][j] == 0) {
-					bfs(i, j);
-
+		for(int r=0; r<N; r++) {
+			for(int c=0; c<M; c++) {
+				if(!visited[r][c] && map[r][c] == 0) {
+					bfs(r, c, 0);
+					
+					// 방문 초기화
+					visited = new boolean[N][M];
 				}
+			
 			}
 		}
 		System.out.println(max);
 		sc.close();
+
 	}
 
-	private static void bfs(int i, int j) {
-		Queue<Water> q = new LinkedList<>();
+	private static void bfs(int R, int C, int Cnt) {
+		Queue<Node> q = new LinkedList();
 		
-		visited[i][j] = true;
-		q.offer(new Water(i, j, 0));
+		visited[R][C] = true;
+		q.offer(new Node(R, C, Cnt));
 		
 		while(!q.isEmpty()) {
-			Water current = q.poll();
-			int r = current.r;
-			int c = current.c;
-			int cnt = current.cnt;
+			Node pos = q.poll();
+			int r = pos.r;
+			int c = pos.c;
+			int cnt = pos.cnt;
+			
+			// 탈출조건
+			// 1일때 최댓값을 구하고 탈출
+			if(map[r][c] == 1) {
+				max = Math.max(cnt, max);
+				return;
+			}
 			
 			for(int d=0; d<8; d++) {
 				int nr = r + dr[d];
 				int nc = c + dc[d];
 				
-				if(nr>=0 && nr<=N-1 && nc>=0 && nc<=M-1 && !visited[nr][nc]) {
+				if(nr>=0 && nr <=N-1 && nc>=0 && nc<=M-1 && !visited[nr][nc]) {
 					visited[nr][nc] = true;
-					q.offer(new Water(nr, nc, cnt+1));
-					if(map[nr][nc] == 1) {
-						max = Math.max(cnt, max);
-						return;
-					}
-					
+					q.offer(new Node(nr, nc, cnt + 1));
 				}
 			}
 		}
-		
 	}
 
 }
