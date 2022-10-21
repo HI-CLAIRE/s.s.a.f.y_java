56a13f5af0aaf180e3812573fcb3dec3a79702b6
gyuyekim <8080claire@naver.com>
2022-10-21 18:05:02 +0900

    221021

--- java/ProblemSolving/src/BOJ/방배정_13300.java
+++ java/ProblemSolving/src/BOJ/방배정_13300.java
@@ -15,7 +15,6 @@ public class 방배정_13300 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
 		int N = sc.nextInt();
 		int K = sc.nextInt();
 		int[][] arr = new int[N][2];
